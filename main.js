f71b8e86170f20b1b88c14386fe6439f8a256519
paulodelosrey <paulodelosrey@gmail.com>
2024-01-02 13:29:23 +0000

    se añade mensaje de api

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// Add JS here
+fetch("https://type.fit/api/quotes")
+  .then(response => response.json())
+  .then(data => {
+    setInterval(function(){
+        document.getElementById("espacio").textContent = data[Math.floor(Math.random() * data.length)].text;
+    }, 5000)
+    
+  })
