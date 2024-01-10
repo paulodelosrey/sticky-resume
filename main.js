832120301b92a3582c9b759dc0bd015345b10eda
paulodelosrey <paulodelosrey@gmail.com>
2024-01-10 22:42:09 +0000

    avance sticky note

--- main.js
+++
@@ -1,8 +0,0 @@
-fetch("https://type.fit/api/quotes")
-  .then(response => response.json())
-  .then(data => {
-    setInterval(function(){
-        document.getElementById("espacio").textContent = data[Math.floor(Math.random() * data.length)].text;
-    }, 5000)
-    
-  })
